edea8f6f61d8c25b3d75147d9ffdf88ce9842ff5
Janae Quinones <janaemassa@gmail.com>
2020-08-23 06:19:40 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,7 +8,7 @@ def using_while
 end
 
 
-# Fill out the content of the `using_while` method so that calling it will 
-#`puts` the desired phrase while your levitation force is less than `10`. 
-#Remember, every time you `puts` the phrase, you should increment your 
+# Fill out the content of the `using_while` method so that calling it will
+#`puts` the desired phrase while your levitation force is less than `10`.
+#Remember, every time you `puts` the phrase, you should increment your
 #levitation force by `1`.
